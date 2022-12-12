66911cf50db5cce97df0d3834a6d3679b043c628
chbert <65483804+chbert@users.noreply.github.com>
2022-12-12 13:22:27 +0100

    env fixes 2

--- src/lib/db/server/index.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { env } from '$env/dynamic/public'
-import { env as envPrivate } from '$env/dynamic/private'
-
-export const supabaseAdminClient = createClient(
-	env.PUBLIC_SUPABASE_URL,
-	envPrivate.PRIVATE_SUPABASE_SERVICE_KEY
-)
