9c87408810841a4048e055547b35f29d6aa3c2bf
chbert <65483804+chbert@users.noreply.github.com>
2022-12-12 13:49:29 +0100

    fix for turnstile

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,33 +4,50 @@ import type { Actions } from './$types'
 import { supabaseAdminClient } from '$db/server'
 import { PRIVATE_TURNSTILE_SECRET_KEY } from '$env/static/private'
 
+interface TokenValidateResponse {
+	'error-codes': string[]
+	success: boolean
+	action: string
+	cdata: string
+}
+
+async function validateToken(token: string, secret: string) {
+	const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded'
+		},
+		body: new URLSearchParams({
+			response: token,
+			secret: secret
+		})
+	})
+
+	const data: TokenValidateResponse = await response.json()
+
+	return {
+		// Return the status
+		success: data.success,
+
+		// Return the first error if it exists
+		error: data['error-codes']?.length ? data['error-codes'][0] : null
+	}
+}
+
 export const actions: Actions = {
 	default: async ({ request }) => {
 		const form = await request.formData()
 		const url = form.get('url')
 
-		const token = String(form.get('cf-turnstile-response'))
-		const ip = String(request.headers.get('CF-Connecting-IP'))
-
-		form.append('secret', PRIVATE_TURNSTILE_SECRET_KEY)
-		form.append('response', token)
-		form.append('remoteip', ip)
-
-		const turnstileUrl = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
-		const result = await fetch(turnstileUrl, {
-			body: form,
-			method: 'POST'
-		})
-
-		const outcome = await result.json()
-
-		console.log('outcome :>> ', outcome)
+		const token = form.get('cf-turnstile-response')
+		const { success, error } = await validateToken(token, PRIVATE_TURNSTILE_SECRET_KEY)
+		if (!success) return fail(400, { message: error })
 
 		// Regex to validate URL
 		const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/
 		const urlIsValid = urlRegex.test(String(url))
 
-		if (urlIsValid && outcome.success) {
+		if (urlIsValid && success) {
 			const { error } = await supabaseAdminClient.from('submissions').insert({ url: url })
 			if (error) return json$1({ error: error?.message }, { status: 500 })
 			return { success: true }
