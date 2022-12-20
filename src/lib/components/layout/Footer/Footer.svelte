c59fe335fee6eb4218ad2f5963da0bf5267b8e32
chbert <65483804+chbert@users.noreply.github.com>
2022-12-20 17:13:15 +0100

    mobile fixes

--- src/lib/components/layout/Footer/Footer.svelte
+++ src/lib/components/layout/Footer/Footer.svelte
@@ -3,42 +3,49 @@
 </script>
 
 <div class="footer">
-	<div class="grid gap-4">
-		<div class="row gy-4">
-			<div class="start col-xl">
-				by <a href="https://christian.bertschy.me">CB</a> <span class="pipe">|</span> original data
-				via
-				<a href="https://sveltesociety.dev/">Svelte Society</a> <span class="pipe">|</span>
-				data updated every 24h
-			</div>
+	<div class="start">
+		by <a href="https://christian.bertschy.me">CB</a>
+		<span class="pipe">|</span>
+		original data via
+		<a href="https://sveltesociety.dev/">Svelte Society</a>
+		& <a href="https://svelte.substack.com/">The (Unofficial) Svelte JS Newsletter</a>
+		<span class="pipe">|</span>
+		data updated every 24h
+	</div>
 
-			<div class="end col-xl">
-				<span class="icon-links">
-					<a
-						href="https://github.com/chbert/svelte-vault"
-						target="_blank"
-						rel="noreferrer"
-						name="Github"
-					>
-						<Github height={24} />
-					</a>
-				</span>
-			</div>
-		</div>
+	<div class="end">
+		<span class="icon-links">
+			<a
+				href="https://github.com/chbert/svelte-vault"
+				target="_blank"
+				rel="noreferrer"
+				name="Github"
+			>
+				<Github height={24} />
+			</a>
+		</span>
 	</div>
 </div>
 
 <style lang="postcss">
 	.footer {
-		padding-top: 1rem;
-		padding-bottom: 1rem;
+		padding: 1rem;
+		gap: 1rem;
 
-		.row {
-			border-top: 1px solid var(--muted-border-color);
+		border-top: 1px solid var(--muted-border-color);
 
-			& .start {
-				color: var(--muted-color);
-			}
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		& .start {
+			color: var(--muted-color);
+			flex-basis: auto;
+		}
+
+		& .end {
+			color: var(--muted-color);
+			flex-basis: 0;
 		}
 
 		& .pipe {
